751f0a2daaa38ec854ae5282e7b3bc9d1fe036c6
Monika Dyrka <dyrka.monika@gmail.com>
2020-08-17 19:12:42 +0200

    add functionality

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 const firstNumber = document.querySelector('.firstNumber');
 const secondNumber = document.querySelector('.secondNumber');
 const result = document.querySelector('.result');
+const answer = document.querySelector('.answer');
 const btn1 = document.querySelector('.reload');
 const btn2 = document.querySelector('.send');
-let a = firstNumber.value;
-let b = secondNumber.value;
+const info = document.querySelector('.info');
+let a;
+let b;
 
-const reload = () => {
-    a = Math.floor(Math.random() * 10);
-    b = Math.floor(Math.random() * 10);
+
+const reloadFunction = () => {
+    result.textContent = '';
+    answer.value = '';
+    info.classList.remove('correct');
+    info.classList.remove('wrong');
+    info.textContent = '';
+    a = Math.floor(Math.random() * 100);
+    b = Math.floor(Math.random() * 100);
+    firstNumber.textContent = a;
+    secondNumber.textContent = b;
 };
 
-btn1.addEventListener('click', reload);
+btn1.addEventListener('click', reloadFunction);
 
-// const add = () => {
-//     a = 
-//     a = firstNumber.value *1;
-//     b = secondNumber.value *1;
-//     sum = a + b;
-//     result.textContent = `${a} plus ${b} gives ${sum}`;
-//     firstNumber.value = '';
-//     secondNumber.value = '';
-// } 
+const add = () => {
+    c = firstNumber.textContent *1;
+    d = secondNumber.textContent *1;
+    sum = c + d;
+    result.textContent = sum;
+    firstNumber.value = '';
+    secondNumber.value = '';
+    if (answer.value == sum) {
+        info.textContent = `Poprawna odpowiedź`;
+        info.classList.add('correct');
+    } else {
+        info.textContent = `Niestety, spróbuj jeszcze raz`;
+        info.classList.add('wrong');
+    }
+} 
 
-// btn2.addEventListener('click', add);
+btn2.addEventListener('click', add);
